46c1ecd76125d9c1d91b0fa2957d0335880b4be1
lnrdgnwn <leonardogunawan15@gmail.com>
2025-07-13 14:41:21 +0700

    fix(api): remove unused api and improve api

--- src/services/ColorAPI.js
+++ src/services/ColorAPI.js
@@ -1,48 +1,45 @@
 import axios from "axios";
 
-// ColorMind API
-async function getRandomRGBColors() {
-  const response = await axios.post(
-    "http://colormind.io/api/",
-    JSON.stringify({ model: "default" }),
-    {
-      headers: { "Content-Type": "text/plain" },
-    }
-  );
-  console.log(response.data.result);
-  return response.data.result;
+// Buat 1 warna acak
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return { r, g, b };
 }
 
-// RGB to Hex Convert Function
-function rgbToHex(r, g, b) {
+// Konversi RGB ke HEX
+function rgbToHex({ r, g, b }) {
   return `#${[r, g, b].map((v) => v.toString(16).padStart(2, "0")).join("")}`;
 }
 
-// TheColorAPI
-async function getNamedColors(rgbColors) {
-  return await Promise.all(
-    rgbColors.map(async ([r, g, b]) => {
-      const hex = rgbToHex(r, g, b);
-      try {
-        const response = await axios.get(
-          `https://www.thecolorapi.com/id?rgb=rgb(${r},${g},${b})`
-        );
-        const name = response.data.name.value;
-        return { r, g, b, hex, name };
-      } catch {
-        return { r, g, b, hex, name: "Unknown" };
-      }
-    })
-  );
+// Ambil nama warna dari TheColorAPI
+async function getColorWithName({ r, g, b }) {
+  try {
+    const response = await axios.get(
+      `https://www.thecolorapi.com/id?rgb=rgb(${r},${g},${b})`
+    );
+    return {
+      r,
+      g,
+      b,
+      hex: response.data.hex.value,
+      name: response.data.name.value,
+    };
+  } catch {
+    return {
+      r,
+      g,
+      b,
+      hex: rgbToHex({ r, g, b }),
+      name: "Unknown",
+    };
+  }
 }
 
 // Main Function
 export async function fetchColorPalettesWithName() {
-  try {
-    const rgbColors = await getRandomRGBColors();
-    const namedColors = await getNamedColors(rgbColors);
-    return namedColors;
-  } catch (error) {
-    console.error("Gagal mengambil palet warna:", error);
-  }
+  const randomColors = Array.from({ length: 5 }, () => getRandomColor());
+  const namedColors = await Promise.all(randomColors.map(getColorWithName));
+  return namedColors;
 }
